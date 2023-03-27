0f310c8a5f2f8da145cdbc5fdfa10f9567cf2b4f
Bruno Abreu <bmachadorj@gmail.com>
2023-03-27 20:43:06 +0100

    ECS Cluster and ECS Task Definition

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.57"
     }
   }
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket = "dbt-tf-poc"
-    key = "dbt-tf-poc-state"
+    key    = "dbt-tf-poc-state"
     region = "eu-west-1"
   }
 }
@@ -18,17 +18,39 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
   default_tags {
-   tags = {
-     project = "dbt_poc"
-   }
- }
+    tags = {
+      project = "dbt_poc"
+    }
+  }
 }
 
 resource "aws_ecr_repository" "dbt_poc" {
-  name = "dbt_poc_repo"
+  name         = "dbt_poc_repo"
   force_delete = true
 }
 
 resource "aws_secretsmanager_secret" "snowflake_password" {
   name = "dbt_poc_snowflake_password"
+}
+
+resource "aws_ecs_cluster" "dbt_poc_cluster" {
+  name = "dbt-poc-cluster"
+}
+
+resource "aws_ecs_task_definition" "dbt_poc_task" {
+  family                   = "dbt-poc-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "dbt-poc-container",
+    "image": "981619280753.dkr.ecr.eu-west-1.amazonaws.com/dbt_poc_repo:latest",
+    "cpu": 1024,
+    "memory": 2048
+  }
+]
+TASK_DEFINITION
 }
