22f799eb51e0df3e8fcd23a9255587759982c306
Bruno Abreu <bmachadorj@gmail.com>
2023-03-27 22:37:17 +0100

    ECS Task Container Role (allow get secret value)

--- main.tf
+++ main.tf
@@ -58,6 +58,38 @@ resource "aws_iam_role" "dbt_poc_task_exec_role" {
   })
 }
 
+resource "aws_iam_role" "dbt_poc_task_container_role" {
+  name               = "dbt-poc-task-container-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "dbt_poc_task_container_role_policy" {
+  name   = "dbt-poc-task-container-role-policy"
+  role   = aws_iam_role.dbt_poc_task_container_role.id
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action   = "secretsmanager:GetSecretValue"
+        Resource = aws_secretsmanager_secret.snowflake_password.arn
+        Effect   = "Allow"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "dbt_poc_task_exec_role_policy_attachment" {
   role       = aws_iam_role.dbt_poc_task_exec_role.name
   policy_arn = data.aws_iam_policy.ecs_task_aws_managed_policy.arn
@@ -76,18 +108,21 @@ resource "aws_ecs_task_definition" "dbt_poc_task" {
   cpu                      = 1024
   memory                   = 2048
   execution_role_arn       = aws_iam_role.dbt_poc_task_exec_role.arn
-  container_definitions    = jsonencode([{
-    name = "dbt-poc-container"
-    image = "981619280753.dkr.ecr.eu-west-1.amazonaws.com/dbt_poc_repo:latest"
-    cpu = 1024
-    memory = 2048
-    logConfiguration = {
-      logDriver = "awslogs"
-      options = {
-        awslogs-group = aws_cloudwatch_log_group.dbt_poc_task_log_group.name
-        awslogs-region = "eu-west-1"
-        awslogs-stream-prefix = "dbt_poc"
+  task_role_arn            = aws_iam_role.dbt_poc_task_container_role.arn
+  container_definitions    = jsonencode([
+    {
+      name             = "dbt-poc-container"
+      image            = "981619280753.dkr.ecr.eu-west-1.amazonaws.com/dbt_poc_repo:latest"
+      cpu              = 1024
+      memory           = 2048
+      logConfiguration = {
+        logDriver = "awslogs"
+        options   = {
+          awslogs-group         = aws_cloudwatch_log_group.dbt_poc_task_log_group.name
+          awslogs-region        = "eu-west-1"
+          awslogs-stream-prefix = "dbt_poc"
+        }
       }
     }
-  }])
+  ])
 }
